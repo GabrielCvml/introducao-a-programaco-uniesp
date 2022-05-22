4ec6ce13e36b02898c7fd8275c07e1ec067bf98a
Gabriel Cavalcante <103397042+GabrielCvml@users.noreply.github.com>
2022-05-22 18:11:24 -0300

    Add files via upload

---
+++ ted11.py
@@ -0,0 +1,33 @@
+from random import randint
+
+matriz = []
+l = soma = 0
+for i in range(1,11):
+    n = []
+for j in range(1,11):
+    numero = randint(10,633)
+    soma = soma + numero 
+    n.append(numero)
+print(matriz)
+l += 1
+matriz.appent(n)
+
+print('------------------------------------------')
+
+soma_t = 0
+for linha in m:
+    for coluna in linha:
+        soma_t = soma_t +coluna
+print(f' O valor total da soma é {soma_t}')
+
+print('------------------------------------------')
+
+f = []
+for linha in matriz:
+  g = []
+  for coluna in linha:
+    numero_3 = coluna*3
+    g.append(numero_3)
+  f.append(g)
+
+  print(g)
